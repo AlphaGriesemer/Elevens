834cde61b484278bc3600060d36e4827f0b3d31a
Alex Griesemer <47705026+AlphaGriesemer@users.noreply.github.com>
2020-09-10 15:15:46 -0400

    Card

--- src/Deck.java
+++ src/Deck.java
@@ -1,3 +1,11 @@
-public class Deck {
+import java.util.ArrayList;
 
+public class Deck {
+    private ArrayList<Card> deck = new ArrayList<>();
+    public Deck(){
+
+    }
+    public int getNumOfCardLeft(){
+        return deck.size();
+    }
 }
