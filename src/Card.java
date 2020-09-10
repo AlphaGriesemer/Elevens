20177f9f66afcb97fd572c27f15a6b010d168d9b
Alex Griesemer <47705026+AlphaGriesemer@users.noreply.github.com>
2020-09-10 15:10:27 -0400

    Card

--- src/Card.java
+++ src/Card.java
@@ -1,8 +1,27 @@
+import java.util.Random;
 public class Card {
     private int value;
+    Random random = new Random();
+    public int getValue(String value){
+        //if A; value = 1
+        //else if T; value = 10
+        return this.value;
+    }
 
-    public int getValue(){
-        return 0;
+    public String create_card(){
+        //int value = random.nextInt(14);
+        //if value > 10: sudo values
+            //if value == 11
+                //J
+            //else if value == 12
+                //Q
+            //else if value == 13
+                //K
+        //else if value == 1
+            //A
+        //else
+            // return string.valueOf(value)
+        return "";
     }
 
 }
